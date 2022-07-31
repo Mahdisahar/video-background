532a539f6458ad03d6baa72e2d60098ecbeba647
sahar <bellem75@yahoo.com>
2022-07-31 15:39:47 -0700

    adding

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+
+const video = document.querySelector('.video-container');
+const btn = document.querySelector('.switch-btn');
+
+btn.addEventListener('click', function() {
+if (!btn.classList.contains('slide')) {
+    btn.classList.add('slide');
+    video.play();
+} else {
+    btn.classList.remove('slide');
+    video.pause();
+}
+});
+
+const preloader = document.querySelector('.preloader');
+window.addEventListener('load', function(){
+preloader.classList.add('hide-preloader');
+});
